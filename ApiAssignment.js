25a7a1c02415709a267843ef295df9587b0b72e5
paulappz <you@example.com>
2016-05-20 19:12:46 +0100

    Api to get pics

--- ApiAssignment.js
+++ ApiAssignment.js
@@ -1,8 +1,19 @@
 
-// This javascript code uses get to retrieve Website HTTP status code from Codeacademy
-var xhr = new XMLHttpRequest();
-xhr.open("GET", "https://www.codecademy.com/", false);
-// Add your code below!
-xhr.send();
-console.log("COde academy website Status code: "+ xhr.status);
-console.log("Status:"+xhr.statusText);
+// This javascript and html code the four most recent pictures of Mount Rainier from the Flickr JSONP A<script>
+(function() {
+  var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+  $.getJSON( flickerAPI, {
+    tags: "mount rainier",
+    tagmode: "any",
+    format: "json"
+  })
+    .done(function( data ) {
+      $.each( data.items, function( i, item ) {
+        $( "<img>" ).attr( "src", item.media.m ).appendTo( "#images" );
+        if ( i === 3 ) {
+          return false;
+        }
+      });
+    });
+})();
+ 
